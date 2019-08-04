aa2ecebb76d7459e7cc2b67118f3eacfdf8c58e5
tdaira <asada09403@gmail.com>
2019-08-04 15:42:24 +0900

    Use HashMap in road construction.

--- src/0403/main.rs
+++ src/0403/main.rs
@@ -1,29 +1,35 @@
 use std::io::stdin;
+use std::collections::HashMap;
 
 struct Point {
+    id: i64,
     x: i64,
     y: i64,
 }
 
 fn calc(point_list: &mut Vec<Point>) -> i64 {
     let mut dist_list = Vec::new();
-    let mut point_list_tmp: Vec<&Point> = point_list.iter().collect();
-    for finish_point in 1..point_list_tmp.len() {
+    let mut point_list_map: HashMap<i64, &Point> =
+        point_list.iter().map(|point| (point.id, point) ).collect();
+    let mut finished_list = Vec::new();
+    // Remove single key.
+    let point = point_list_map.remove(&point_list[0].id).unwrap();
+    finished_list.push(point);
+    while !point_list_map.is_empty() {
         let mut min: i64 = std::i64::MAX;
-        let mut unfinished_index  = 0;
-        for i in 0..finish_point {
-            let finished = point_list_tmp[i];
-            for j in finish_point..point_list_tmp.len() {
-                let unfinished = point_list_tmp[j];
+        let mut min_point_id = None;
+        for finished in &finished_list {
+            for unfinished in point_list_map.iter().map(|t| t.1) {
                 let dist = get_dist(finished, unfinished);
                 if min > dist {
                     min = dist;
-                    unfinished_index = j;
+                    min_point_id = Option::Some(unfinished.id);
                 }
             }
         }
         dist_list.push(min);
-        point_list_tmp.swap(finish_point, unfinished_index);
+        finished_list.push(point_list_map[&min_point_id.unwrap()]);
+        point_list_map.remove(&min_point_id.unwrap());
     }
     dist_list.iter().sum()
 }
@@ -43,12 +49,12 @@ fn main() {
     stdin().read_line(&mut s).unwrap();
     let lines: i64 = s.trim().parse().unwrap();
     let point_list: &mut Vec<Point> = &mut Vec::new();
-    for _ in 0..lines {
+    for i in 0..lines {
         let mut s = String::new();
         stdin().read_line(&mut s).unwrap();
         let vals: Vec<i64> = s.trim().split_whitespace()
             .map(|e| e.parse().unwrap()).collect();
-        point_list.push(Point{ x: vals[0], y: vals[1]});
+        point_list.push(Point{ id: i, x: vals[0], y: vals[1]});
     }
     println!("{:?}", calc(point_list));
 }
